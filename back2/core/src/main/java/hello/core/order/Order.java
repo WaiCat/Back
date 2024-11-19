34b3ee296322bbcbce781bc659b4216d17bc57bd
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-11-20 00:01:32 +0900

    back study

--- back2/core/src/main/java/hello/core/order/Order.java
+++ back2/core/src/main/java/hello/core/order/Order.java
@@ -53,7 +53,7 @@ public class Order {
     @Override
     public String toString() {
         return "Order{" +
-                "memverId=" + memberId +
+                "memberId=" + memberId +
                 ", itemName='" + itemName + '\'' +
                 ", itemPrice=" + itemPrice +
                 ", discountPrice=" + discountPrice +
